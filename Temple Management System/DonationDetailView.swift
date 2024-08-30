b8b15929e33a5e724c64aa1166270efd1007e5a3
ripalashah <ripalshah@yahoo.com>
2024-08-30 09:03:54 -0400

    3 files new created and DonationManagementView.swift changed for edit and list view functionality

---
+++ Temple Management System/DonationDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DonationDetailView.swift
+//  Temple Management System
+//
+//  Created by Ripal Shah on 8/30/24.
+//
+
+import Foundation
---
+++ Temple Management System/DonationRow.swift
@@ -0,0 +1,8 @@
+//
+//  DonationRow.swift
+//  Temple Management System
+//
+//  Created by Ripal Shah on 8/30/24.
+//
+
+import Foundation
